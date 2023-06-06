cff9e32fbba955694a080ee9e0f9969d400fb05d
i-saac <iwegner123@gmail.com>
2023-06-05 20:51:29 -0700

    Added Linear Interpolation Function

--- src/vector.rs
+++ src/vector.rs
@@ -15,6 +15,8 @@ impl<T> ApplyFunction<T> for Vec<T> {
 }
 
 /// Discrete trapezoidal integration of dep with respect to ind
+/// 
+/// Assumes sorted inputs
 pub fn integrate(ind: &Vec<f32>, dep: &Vec<f32>) -> SciResult<Vec<f32>> {
     if ind.len() != dep.len() {
         return Err(SciError::VectorLengthsError)
@@ -28,4 +30,29 @@ pub fn integrate(ind: &Vec<f32>, dep: &Vec<f32>) -> SciResult<Vec<f32>> {
     }
 
     Ok(output_vec)
+}
+
+/// Estimate value of dependent at input extrapolated from ind and dep
+/// 
+/// Assumes sorted inputs
+pub fn linterp(ind: &Vec<f32>, dep: &Vec<f32>, input: f32) -> SciResult<f32> {
+    if ind.len() != dep.len() {
+        return Err(SciError::VectorLengthsError)
+    }
+
+    if ind[0] > input || input > ind[ind.len() - 1] {
+        return Err(SciError::RangeError)
+    }
+
+    let mut idx: usize = 0;
+    while ind[idx] < input { idx += 1 }
+
+    let less_idx: usize = idx - 1;
+
+    let d_ind: f32 = ind[idx] - ind[less_idx];
+    let d_dep: f32 = dep[idx] - dep[less_idx];
+
+    let guess_dep: f32 = dep[less_idx] + (d_dep / d_ind) * (input - ind[less_idx]);
+
+    Ok(guess_dep)
 }
